22d88c6f935ed1816b36ea2d2d935a1eca4d51a4
Filip Stanciu <filipstanciu2005@gmail.com>
2026-08-18 11:57:11 +0300

    Configure Azure deployment

--- backend/src/config/environment.ts
+++ backend/src/config/environment.ts
@@ -4,4 +4,10 @@ export const ENV = {
   TEMP_DIR: process.env.TEMP_DIR || "temp",
 
   YT_DLP_PATH: process.env.YT_DLP_PATH ?? "yt-dlp",
+
+  JS_RUNTIME:
+    process.env.JS_RUNTIME ??
+    (process.platform === "win32"
+      ? "node"
+      : "node:/usr/bin/node"),
 };
